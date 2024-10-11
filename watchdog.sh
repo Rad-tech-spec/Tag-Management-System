1f4b826516a6677665911d5bc26b64234449f6eb
Rad Eshghi <rad.eshghi@actemium.com>
2024-10-11 13:44:45 -0400

    Last Commit

--- watchdog.sh
+++ watchdog.sh
@@ -14,15 +14,28 @@
 #     fi
 #     sleep 5  # Check every 5 seconds
 # done
-#!/bin/bash
 
-# Check if the cron job is running
-while true; do
-    if ! pgrep -f "python3 res_sarnia/src/main.py" > /dev/null; then
-        echo "Cron job not running. Starting it..."
-        python3 res_sarnia/src/main.py &  # Replace with your cron job command
-    else
-        echo "Cron job is running."
-    fi
-    sleep 300
-done
+# # Check if the cron job is running
+# while true; do
+#     if pidof -o %PPID -x "$0"; then
+#         echo "Cron job is running."
+#     else
+#         echo "Cron job not running. Starting it..."
+#         python3 res_sarnia/src/main.py &  # Replace with your cron job command
+#     fi
+#     sleep 300
+# done
+
+# Check if the script is already running
+if pidof -o %PPID -x "$0"; then
+    echo "Script is already running." >> /home/actemiumsumma/Historian-Sarnia/crontaboutput.txt
+    exit 1
+else 
+    echo "Script not running! Starting..." >> /home/actemiumsumma/Historian-Sarnia/crontaboutput.txt
+    # Long-running task
+    while true; do
+        echo "Job running." >> /home/actemiumsumma/Historian-Sarnia/crontaboutput.txt
+        python3 res_sarnia/src/main.py & # Replace with your cron job command
+        sleep 300
+    done
+fi
